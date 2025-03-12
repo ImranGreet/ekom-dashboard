a68433c9f598b73d5cde66228f4134920666328f
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2025-03-12 21:33:53 +0600

    home page tiles are added

--- src/app/components/custom/tiles/tiles.component.ts
+++ src/app/components/custom/tiles/tiles.component.ts
@@ -1,9 +1,12 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-tiles',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,IconField,InputIcon],
   templateUrl: './tiles.component.html',
   styleUrl: './tiles.component.scss'
 })
